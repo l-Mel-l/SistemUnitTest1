df26693f078b2238ea479ec8524c65b61c9045da
l-Mel-l <125726970+l-Mel-l@users.noreply.github.com>
2023-10-16 15:46:27 +0300

    Добавленно тестирование метода main

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
@@ -16,5 +19,24 @@ public class MainTest {
         assertEquals(expected1,actual1);
         assertEquals(expected2,actual2);
         assertEquals(expected3,actual3);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalOut = System.out;
+        System.setOut(printStream);
+
+        // Выполнение метода main()
+        Main.main(new String[]{});
+
+        // Получение результатов вывода на консоль
+        String consoleOutput = outputStream.toString();
+
+        // Восстановление стандартного вывода на консоль
+        System.setOut(originalOut);
+
+        // Проверка ожидаемых значений
+        assertTrue(consoleOutput.contains("Значение выражения 1: 14"));
+        assertTrue(consoleOutput.contains("Значение выражения 2: 2"));
+        assertTrue(consoleOutput.contains("Значение выражения 3: 5040"));
     }
 }
+
